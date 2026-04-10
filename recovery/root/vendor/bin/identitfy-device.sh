efc226a90aae9a7969f102dd80090c5ce017d694
adbzxcghf <adbzxcghf@gmail.com>
2026-04-10 22:46:40 +0800

    Update identitfy-device.sh

--- recovery/root/vendor/bin/identitfy-device.sh
+++ recovery/root/vendor/bin/identitfy-device.sh
@@ -1,5 +1,15 @@
 #!/system/bin/sh
 
+is_PEXM00() {
+    echo "Setup props for PEXM00"
+    resetprop "ro.build.product" "PEXM00"
+    resetprop "ro.product.model" "PEXM00"
+    resetprop "ro.product.device" "OP4E9F"
+    resetprop "ro.product.product.device" "OP4E9F"
+    resetprop "ro.commonsoft.ota" "OP4E9F"
+    resetprop "ro.separate.soft" "20127"
+}
+
 is_RMX3142() {
     echo "Setup props for RMX3142"
     resetprop "ro.build.product" "RMX3142"
@@ -20,28 +30,18 @@ is_PEGM00() {
     resetprop "ro.separate.soft" "20111"
 }
 
-is_PEXM00() {
-    echo "Setup props for PEXM00"
-    resetprop "ro.build.product" "PEXM00"
-    resetprop "ro.product.model" "PEXM00"
-    resetprop "ro.product.device" "OP4E9F"
-    resetprop "ro.product.product.device" "OP4E9F"
-    resetprop "ro.commonsoft.ota" "OP4E9F"
-    resetprop "ro.separate.soft" "20127"
-}
-
 project=$(cat /proc/oplusVersion/prjName)
 echo $project
 
 case $project in
-"2065C")
-    is_RMX3142
+"20127")
+    is_PEXM00
     ;;
 "20111")
     is_PEGM00
     ;;
-"20127")
-    is_PEXM00
+"2065C")
+    is_RMX3142
     ;;
 *)
     is_RMX3142
